4a55b4b405b34b95c268aada4227a10f2ed95ecd
igustin10 <37119096+igustin10@users.noreply.github.com>
2018-03-06 17:44:58 -0300

    public->img->favico || la morne comm

--- SantaElena/resources/views/layouts/head.blade.php
+++ SantaElena/resources/views/layouts/head.blade.php
@@ -2,6 +2,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>{{config('app.name', 'Santa Elena')}}</title>
-<link rel="icon" href="{{URL::asset('FAVICO.png')}}" type="image/gif" sizes="16x16">
+<link rel="icon" href="{{asset('img/FAVICO.png')}}" type="image/gif" sizes="16x16">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
